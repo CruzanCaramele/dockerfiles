97b2ed54812c6cea2312501762bda97c551c322f
Hamza Shehu <letters2hamza@gmail.com>
2016-12-24 19:03:47 +0800

    css files

---
+++ portfolio/static/scripts/skills.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	$(".python").css("width", "89%")
+	$(".html").css("width", "80%")
+	$(".css").css("width", "79%")
+	$(".javascript").css("width", "70%")
+	$(".jquery").css("width", "71%")
+	$(".flask").css("width", "88%")
+	$(".django").css("width","70%")
+	$(".postgreSQL").css("width", "69%")
+	$(".json").css("width", "55%")
+	$(".sql").css("width", "59%")
+	$(".unix").css("width", "50%")
+	$(".api").css("width", "55%")
+	$(".apache").css("width", "78%")
+	$(".git").css("width", "90%")
+
+	//DEVOPS
+	$(".chef").css("width", "44%")
+	$(".jenkins").css("width", "35%")
+	$(".docker").css("width", "69%")
+	$(".packer").css("width", "60%")
+	$(".oracle").css("width", "79%")
+	$(".vagrant").css("width", "80%")
+
+});
